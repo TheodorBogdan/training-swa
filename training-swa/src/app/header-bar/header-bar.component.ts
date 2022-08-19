cee06fb7d5a177da69a9fc59277ef1af6ad42992
Bogdan Vrancean <bogdan.vrancean@sitline.ro>
2022-08-19 16:53:48 +0300

    added show user details button

--- training-swa/src/app/header-bar/header-bar.component.ts
+++ training-swa/src/app/header-bar/header-bar.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-header-bar',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-bar.component.scss']
 })
 export class HeaderBarComponent implements OnInit {
+  public showModal = false;
+  public message: any = '';
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  showUserDetails(): void {
+    this.http.get(`${environment.apiUrl}/user/current`).subscribe({
+      next: t => {
+        this.message = t;
+        this.showModal = true;
+      }
+    })
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+  }
 }
